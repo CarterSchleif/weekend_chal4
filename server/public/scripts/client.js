9e310c7987038a3be51df3727f7a7341434bc2f1
Carter Schleif <carterschleif@Carters-MacBook-Pro.local>
2018-02-25 13:39:03 -0600

    finished like button and functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,7 @@ console.log('working on client');
 const app = angular.module('myApp',[]);
 const GalleryController = app.controller('GalleryController', ['$http', function($http){
 let self = this;
-
+// let likes1=0;
 // self.addPhotos = function (photoDiv){
 //     console.log('in save button', photoDiv);
 //     $http({
@@ -18,6 +18,9 @@ let self = this;
 //     })
 //   }
 
-
+// self.likesOne = function(){
+//     console.log('in likes1:', likes1);
+//     self.likes1++
+// }
 
 }]);//end GalleryController
